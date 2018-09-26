cca48b39c1bb46d027a0f3ba9b77afa3d11182f8
fan <1270238568@qq.com>
2018-09-26 08:36:36 +0800

    the api of the camera

---
+++ cpp/sampleExportPLY/build/CMakeFiles/sampleExportPLY.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fan/royale/libroyale-3.20.0.62-LINUX-x86-64Bit/samples/cpp/sampleExportPLY/sampleExportPLY.cpp" "/home/fan/royale/libroyale-3.20.0.62-LINUX-x86-64Bit/samples/cpp/sampleExportPLY/build/CMakeFiles/sampleExportPLY.dir/sampleExportPLY.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/fan/royale/libroyale-3.20.0.62-LINUX-x86-64Bit/include"
+  "/home/fan/royale/libroyale-3.20.0.62-LINUX-x86-64Bit/include/royaleCAPI"
+  "../../../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
